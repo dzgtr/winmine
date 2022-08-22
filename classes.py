633ed6960feeeddede5c27f82b00f0ca7afb2ca3
dzgtr <tomas.merth@atlas.cz>
2022-08-22 08:30:25 +0200

    Fully working in command line with classes

--- classes.py
+++ classes.py
@@ -7,7 +7,6 @@ class Field:
         self.isFlagged = False
         self.isClicked = False
 
-
 class Board:
     def __init__(self, size_x, size_y, minecount):
         self.gameboard = []
@@ -19,36 +18,39 @@ class Board:
         for x in range(self.size_y):
             self.gameboard.append([])
             for y in range(self.size_x):
-                self.gameboard[x].append(0)
+                self.gameboard[x].append(Field())
 
     def plant_on_board(self):
         planted_minecount = 0
         while planted_minecount < self.minecount:
             random_row = random.randint(0, self.size_y - 1)
             random_col = random.randint(0, self.size_x - 1)
-            if self.gameboard[random_row][random_col] == 0:
-                self.gameboard[random_row][random_col] = 1
+            if self.gameboard[random_row][random_col].isMine == False:
+                self.gameboard[random_row][random_col].isMine = True
                 planted_minecount += 1
 
     def print_board(self):
         for x in range(len(self.gameboard)):
-            print(self.gameboard[x])
+            for y in range(len(self.gameboard[x])):
+                print(self.gameboard[x][y].isMine, end=" ")
+            print("")
 
     def guess(self, guess_x, guess_y):
-        if self.gameboard[guess_x][guess_y] == 1:
+        if self.gameboard[guess_x][guess_y].isMine == True:
             print("You Lost!")
             return True
-        elif self.gameboard[guess_x][guess_y] == 2:
+        elif self.gameboard[guess_x][guess_y].isClicked == True:
             print("Already clicked here")
         else:
             print("Miss")
-            self.gameboard[guess_x][guess_y] = 2
+            self.gameboard[guess_x][guess_y].isClicked = True
 
     def is_over(self):
-        isover = True
         for y in range(self.size_y):
             for x in range(self.size_x):
-                if self.gameboard[x][y] == 0:
+                if self.gameboard[x][y].isMine or self.gameboard[x][y].isClicked:
+                    isover = True
+                else:
                     isover = False
                     break
         return isover
